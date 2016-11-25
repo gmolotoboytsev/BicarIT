840870b29a1ed112559cbf7704346023aff69f72
George <molotoboytsev94@gmail.com>
2016-11-25 22:06:44 +0300

    prepare for modx

--- js/script.js
+++ js/script.js
@@ -4,4 +4,40 @@ $(document).ready(function() {
     }, {
         resizing: true
     });
+
+    $(".search").on("click", function() {
+      var area = $(".search-area");
+      if (area.css("display") ==  "none") {
+        area.show();
+        $(this).hide();
+      }
+    });
+
+    $(".search-close").on("click", function() {
+        $(".search-area").hide();
+        $(".search").show();
+    });
+
+
+    $(".place-button").on("click", function() {
+        var id = $(this).attr("id");
+        $(".place-area").hide();
+        $(".place-button").removeClass("place-active");
+        $(this).addClass("place-active");
+        switch(id) {
+            case "butOffice":
+                $("#placeOffice").show();
+                break;
+            case "butStorage":
+                $("#placeStorage").show();
+                break;
+        }
+    });
+
+});
+
+
+$(window).scroll(function() {
+    var top = $(document).scrollTop();
+    
 });
